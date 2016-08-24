bff4c65c702dd2308b6ad36ad76e8698d924b638
Thomas Shannon <thomasshannon1117@yahoo.com>
2016-08-24 11:19:39 -0700

    linked style

--- app.js
+++ app.js
@@ -24,20 +24,45 @@ var WIDTH = 1000;
 var HEIGHT = 500;
 var MARGINS = {
         top: 20,
-        right: 20,
-        bottom: 20,
+        right: 40,
+        bottom: 30,
         left: 50
     };
 
 var xScale = d3.scale.linear().range([MARGINS.left, WIDTH - MARGINS.right]).domain([2000,2010]);
 var yScale = d3.scale.linear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([134,215]);
 
+var lineGen = d3.svg.line()
+  .x(function(d) {
+    return xScale(d.year);
+  })
+  .y(function(d) {
+    return yScale(d.sale);
+  })
+
+
 
 var xAxis = d3.svg.axis()
     .scale(xScale);
   
 var yAxis = d3.svg.axis()
-    .scale(yScale);
+    .scale(yScale)
+    .orient("right");
 
 vis.append("svg:g")
+	.attr("class","axis")
+    .attr("transform", "translate(0," + (HEIGHT - MARGINS.bottom) + ")")
     .call(xAxis);
+
+
+vis.append("svg:g")
+	.attr("class","axis")
+    .attr("transform", "translate(" + (WIDTH-MARGINS.right) + ",0)")
+    .call(yAxis);
+
+
+vis.append('svg:path')
+  .attr('d', lineGen(data))
+  .attr('stroke', 'green')
+  .attr('stroke-width', 2)
+  .attr('fill', 'none');
