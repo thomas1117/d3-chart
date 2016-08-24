100a7b25299086e7301f11640ce7325f824c7926
Thomas Shannon <thomasshannon1117@yahoo.com>
2016-08-24 11:32:06 -0700

    added wrapped container

--- app.js
+++ app.js
@@ -19,18 +19,19 @@ var data = [{
     "year": "2010"
 }];
 
-var vis = d3.select("#visualisation");
+var vis = d3.select("#visualization");
 var WIDTH = 1000;
 var HEIGHT = 500;
+var PAD = 20;
 var MARGINS = {
-        top: 20,
+        top: 30,
         right: 40,
         bottom: 30,
         left: 50
     };
 
 var xScale = d3.scale.linear().range([MARGINS.left, WIDTH - MARGINS.right]).domain([2000,2010]);
-var yScale = d3.scale.linear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([134,215]);
+var yScale = d3.scale.linear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([134-PAD,215+PAD]);
 
 var lineGen = d3.svg.line()
   .x(function(d) {
